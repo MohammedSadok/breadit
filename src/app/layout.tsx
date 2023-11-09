3dd50800b63a066aabaca988108cfe9552d6fa07
MohammedSadok <mohammed.sadok.2003@gmail.com>
2023-11-09 10:49:59 +0100

    add auth and create post

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,43 +1,45 @@
-import { cn } from '@/lib/utils'
-import { Inter } from 'next/font/google'
-// import Navbar from '@/components/Navbar'
-// import Providers from '@/components/Providers'
-// import { Toaster } from '@/components/ui/Toaster'
+import { cn } from "@/lib/utils";
+import { Inter } from "next/font/google";
+import Navbar from "@/components/Navbar";
+import Providers from "@/components/Providers";
+import { Toaster } from "@/components/ui/Toaster";
 
-import '@/styles/globals.css'
+import "@/styles/globals.css";
 
-const inter = Inter({ subsets: ['latin'] })
+const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
-  title: 'Breadit',
-  description: 'A Reddit clone built with Next.js and TypeScript.',
-}
+  title: "Breadit",
+  description: "A Reddit clone built with Next.js and TypeScript.",
+};
 
 export default function RootLayout({
   children,
   authModal,
 }: {
-  children: React.ReactNode
-  authModal: React.ReactNode
+  children: React.ReactNode;
+  authModal: React.ReactNode;
 }) {
   return (
     <html
-      lang='en'
+      lang="en"
       className={cn(
-        'bg-white text-slate-900 antialiased light',
+        "bg-white text-slate-900 antialiased light",
         inter.className
-      )}>
-      <body className='min-h-screen pt-12 antialiased bg-slate-50'>
-        {/* <Providers>
+      )}
+    >
+      <body className="min-h-screen pt-12 antialiased bg-slate-50">
+        <Providers>
+          {/* @ts-expect-error Server Component */}
           <Navbar />
-          {authModal} */}
+          {authModal}
 
-          <div className='container h-full pt-12 mx-auto max-w-7xl'>
+          <div className="container h-full pt-12 mx-auto max-w-7xl">
             {children}
           </div>
-        {/* </Providers>
-        <Toaster /> */}
+        </Providers>
+        <Toaster />
       </body>
     </html>
-  )
+  );
 }
