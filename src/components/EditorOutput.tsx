0e6b6c97a5ea9e87e9aade1b49bacdbca6224f20
MohammedSadok <mohammed.sadok.2003@gmail.com>
2023-11-09 21:11:43 +0100

    add redis for cashing

--- src/components/EditorOutput.tsx
+++ src/components/EditorOutput.tsx
@@ -28,9 +28,8 @@ const style = {
 
 const EditorOutput: FC<EditorOutputProps> = ({ content }) => {
   return (
-    // @ts-expect-error
     <Output
-      style={style}
+      style={style} 
       className='text-sm'
       renderers={renderers}
       data={content}
